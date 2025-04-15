74c72f14905f2b66ee484283c863c73bfaa8d3c6
Kyle <kyle@example.com>
2025-04-15 16:57:59 -0400

    Finish commenting main.py, some level ed bufixes

--- main.py
+++ main.py
@@ -449,6 +449,9 @@ class SmallEnemyShip(WorldObject):
     pass
 
   def update(self, delta):
+    if gameManager.scene_name != 'game':
+      return
+    
     # Every 2 seconds, shoot directly ahead
     self.shootCooldown -= delta
     if self.shootCooldown < 0:
@@ -757,13 +760,12 @@ class EnemyMotherShip(WorldObject):
 class Bird(WorldObject):
   def __init__(self, x, y, img="bird.png", flipped=False):
     super().__init__(x, y)
-    self.color = (255, 255, 0)  # Yellow color
     self.size = 0.65
     self.img = renderer.load_image(img)
     self.speed = 5
     self.flipped = flipped
     self.nearestShip = None
-    self.insideTileX = 0
+    self.insideTileX = 0 # Determines which Tile the Bird is under (not used currently)
     self.insideTileY = 0
     self.isNearDoor = False
     self.lastX = x
@@ -778,8 +780,11 @@ class Bird(WorldObject):
     self.y = self.lastY
 
   def player_control(self, delta):
+    # lastX is useful for reverting the position of the bird
     self.lastX = self.x
     self.lastY = self.y
+    
+    # Calculate velocity X and Y based on WASD keys
     vx = 0
     vy = 0
     if keys.get('w', False):
@@ -792,27 +797,31 @@ class Bird(WorldObject):
     if keys.get('d', False):
       vx += self.speed * delta
       self.flipped = True
+      
+    # If debug mode is enabled and the left shift key is pressed, go at super fast speeds
     if DEBUG_MODE and keys.get('Shift_L', False):
       self.speed = 50 # for debuggin
       
+    # Diagonal walking speed scaling
     if abs(vx) and abs(vy):
       vx *= math.sqrt(2) / 2
       vy *= math.sqrt(2) / 2
     
+    # Movement based on velocity
     self.x += vx
     self.y += vy
+    
+    # Press Enter to enter a ship or turret station
     if keys.get('Return', False) and self.nearestShip:
-      # Entering the ship
       keys['Return'] = False
-      # self.x = self.nearestShip.chairX
-      # self.y = self.nearestShip.chairY
       global me
       me = self.nearestShip
       self.x = me.x
       self.y = me.y
       self.nearestShip.cockpit = self
-        
-    if self.follower: # If bird has follower, move the follower towards the bird, keeping the angle but targetting a certain distance
+    
+    # If bird has follower, move the follower towards the bird, keeping the angle but targetting a certain distance
+    if self.follower: 
       # Calculate the angle between the two birds
       dx = self.x - self.follower.x
       dy = self.y - self.follower.y
@@ -822,15 +831,10 @@ class Bird(WorldObject):
       direction = 1 if currentDistance > targetDistance else 0
       self.follower.x += math.cos(angle) * self.speed * 0.85 * direction * delta
       self.follower.y += math.sin(angle) * self.speed * 0.85 * direction * delta
-      self.follower.flipped = dx > 0
+      self.follower.flipped = dx > 0 # Flip the follower based on its position relative to the player
       
-
   def update(self, delta):
-    # if self.insideTile and self.insideTile.parent and not self.insideTile.parent.cockpit:
-    # if self.nearestShip:
-    #   self.x += self.nearestShip.vx * delta
-    #   self.y += self.nearestShip.vy * delta
-
+    # Update `self.nearestShip` to be the nearest ship or turret station within 3 units
     self.nearestShip = None
     minDist = 1000
     for obj in objs:
@@ -839,6 +843,8 @@ class Bird(WorldObject):
         if dist < minDist and dist < 3:
           minDist = dist
           self.nearestShip = obj
+          
+    # Legacy code to calculate in-ship collisions (if I had more time in this hackathon)
 
     # # For nearest ship calculate the tile x and y
     # if self.nearestShip:
@@ -905,17 +911,14 @@ class Bird(WorldObject):
           else:
             self.y = wall.y + self.size
 
-
-
-
   def draw(self):
+    # Draw a normal or flipped image of the bird
     func = renderer.world_img_flipped if self.flipped else renderer.world_img
     func(self.img, self.x - self.size / 2, self.y - self.size / 2, self.size)
 
-    # renderer.img(self.img, self.x, self.y, self.size)
-
 
 class Decoration(WorldObject):
+  # Very basic object - can be a tree or a book or something similar
   def __init__(self, x, y, decorIndex=0):
     super().__init__(x, y)
     self.decorIndex = decorIndex
@@ -926,6 +929,7 @@ class Decoration(WorldObject):
     pass
 
   def updatePoints(self):
+    # Used to select a specific asset in the level editor
     self.decorIndex = min(max(0, self.decorIndex), 3)
     self.image = renderer.load_image(f'decor-{self.decorIndex}.png')
 
@@ -935,6 +939,7 @@ class Decoration(WorldObject):
 
 
 class RectRoom(WorldObject):
+  # Rectangle room - no collisions, just a rectangle
   def __init__(self, x, y, x2=None, y2=None):
     super().__init__(x, y)
     self.x2 = x2
@@ -951,7 +956,6 @@ class RectRoom(WorldObject):
       self.y, self.y2 = self.y2, self.y
 
   def draw(self):
-
     if self.x2 == None:
       renderer.world_rect(self.x, self.y, 1, 1, self.color)
     else:
@@ -965,6 +969,8 @@ class StoryController:
     self.inDialog = True
     self.currentDialog = []
     self.startDialog = [
+      # [speaker, text]
+      # "bird2" is the internal code name for Albert, your companion
       ["bird2", "Welcome to Birds in Space!"],
       ["me", "Where am I?"],
       ["bird2", "You are on our home ship.\nWe are in the middle of an epic\nspace battle.\nWe need your help to win."],
@@ -1000,12 +1006,14 @@ class StoryController:
       ["system", "You have won the battle."],
       ["system", "Thanks for playing!"]
     ]
+    # Hint text is the text that diplays along the bottom of the screen
     self.hintText = ""
     self.setCurrentDialog(self.startDialog)
-    self.stage = "EXPLORE"
+    self.stage = "EXPLORE" # Story stage
     self.interactedWithBook = False
-    self.octagonRef = None
+    self.octagonRef = None # For caching
 
+  # Set the current dialog
   def setCurrentDialog(self, dialogObj, hintText="Click to advance story"):
     self.setHintText(hintText)
     self.currentDialog = dialogObj
@@ -1016,7 +1024,8 @@ class StoryController:
     self.hintText = text
     
   def makeObjectsIndestructible(self):
-    # Make shields indestructible
+    # Make shields indestructible and the enemy ship - makes the game impossible to win unless
+    #   the player progresses in the story
     for obj in objsWithHP:
       if isinstance(obj, Shields):
         obj.hp = obj.maxHP
@@ -1026,8 +1035,8 @@ class StoryController:
   def update(self, delta):
     if gameManager.scene_name != 'game':
       return
-    # print(self.stage, math.dist((me.x, me.y), (bookOfAnswers.x, bookOfAnswers.y)) , self.interactedWithBook, self.hintText)
     
+    # If in a dialog, "freeze" the story
     if self.inDialog:
       if mouse['left'] or keys.get('space', False):
         mouse['left'] = False
@@ -1036,7 +1045,8 @@ class StoryController:
         if self.storyStep > len(self.currentDialog) - 1:
           self.inDialog = False
           self.setHintText("")
-          
+    
+    # Exploring stage
     elif self.stage == 'EXPLORE':
       if not self.octagonRef:
         self.octagonRef = list(filter(lambda obj: isinstance(obj, EnemyMotherShip), objsWithHP))[0]
@@ -1061,10 +1071,10 @@ class StoryController:
             self.askForHelpDialog, "Click to advance story")
           me.follower = bird2
           self.stage = 'PRE-BATTLE-1'
-          
       else:
         self.setHintText("")
     
+    # Walking with bird2 to the hangar
     elif self.stage == 'PRE-BATTLE-1':
       if me.x >= -13 and me.y >= -20 and me.x <= -8 and me.y <= -13:
         self.stage = 'PRE-BATTLE-2'
@@ -1072,17 +1082,18 @@ class StoryController:
           self.hangarInfoDialog, "Click to advance story")
         me.follower = None
     
+    # Bird2 jumps into the ship
     elif self.stage == 'PRE-BATTLE-2':
       bird2.x += (ship2.x - bird2.x) * 0.1
       bird2.y += (ship2.y - bird2.y) * 0.1
       if math.dist((bird2.x, bird2.y), (ship2.x, ship2.y)) < 0.002:
         self.stage = 'BATTLE'
         ship2.shootCooldown = 7
-        
-      me.x = me.lastX # freeze player
+      # freeze player to prevent player from going in bird2's designated ship
+      me.x = me.lastX 
       me.y = me.lastY
       
-        
+    # Main battle phase - it is not possible to win the game
     elif self.stage == 'BATTLE':
       bird2.x = ship2.x + 0.5
       bird2.y = ship2.y + 0.5
@@ -1103,6 +1114,7 @@ class StoryController:
           else:
             ship2.theta -= ship2.rotSpeed * delta
         
+        # Win trigger - defeat the octagon ship
         if o.hp <= 0:
           self.setCurrentDialog(
             self.endDialog, "Click to advance story")
@@ -1114,14 +1126,15 @@ class StoryController:
       # Show controls as a hint
       self.setHintText("A/D to steer, W/S to control thrust, Click to shoot")
     
+    # Make the game impossible to win unless the player progresses in the story
     if self.stage != 'BATTLE' and self.stage != 'POST-BATTLE':
       self.makeObjectsIndestructible()
       
-      
-
   def draw(self):
     if gameManager.scene_name != 'game':
       return
+    
+    # Display dialog box if there is an active dialog ongoing
     if self.inDialog:
       speaker = self.currentDialog[self.storyStep][0]
       text = self.currentDialog[self.storyStep][1]
@@ -1160,21 +1173,28 @@ class LevelEditor:
       ['Rect. Room', None, RectRoom, 'xy-x2-y2'],
       ['Turret Station', 'turret-station.png', TurretStation],
       ['Turret', 'turret.png', Turret],
-      ['Sheilds', None, Shields, 'x-w', 'y-h'],
-
-
+      ['Shields', None, Shields, 'x-w', 'y-h'],
     ]
+    
+    # Load all the images beforehand
     for o in self.options:
       if o[1] != None:
         o[1] = renderer.load_image(o[1])
+        
+    # If currently placing an entity or not
     self.mode = "none"  # "none"|"placing"
+    
+    # Index of the currently hovered option
     self.hovering = -1
+    
+    # Instance of the object being placed
     self.ghost = None
     self.argStep = 0
 
   def update(self, delta):
+    # Press P to generate the level code (paste where it says "Insert Level Code Here!")
     if keys.get('p', False):
-      print('# Level code:')
+      print('\n###### Level code ######')
       keys['p'] = False
       for obj in objs:
         corrospondingOp = None
@@ -1199,11 +1219,8 @@ class LevelEditor:
               print(f", {getattr(obj, arg1)}", end="")
           print(")")
 
-    pass
-
   def draw(self):
-
-    # Draw a grid over everything
+    # Draw a grid over everything, every 4 gridlines for performance reasons
     x1 = x1s = math.floor(renderer.RevX(0))
     y1 = math.floor(renderer.RevY(0))
     x2 = math.floor(renderer.RevX(window['width']))
@@ -1217,17 +1234,19 @@ class LevelEditor:
       renderer.world_line(x1, y1, x2, y1, '#222', renderer.RevS(gridlineWidth))
       y1 += 4
 
+    # Draw the object being placed down
     op = self.options[self.hovering]
     if self.mode == "none":
       pass
     elif self.mode == "placing":
       if self.argStep == 0:
+        # Stamping down an object for the first time (setting its X and Y coordinates)
         self.ghost.x = round(renderer.RevX(mouse['x']))
         self.ghost.y = round(renderer.RevY(mouse['y']))
         renderer.text(
           10, 140, f"Click to place\nX: {self.ghost.x}\nY: {self.ghost.y}", 'white')
       else:
-        # print(op, self.argStep)
+        # Setting secondary arguments (like size, x2, y2, etc.)
         dir, arg, *arg2 = op[self.argStep + 2].split("-")
         s = 0
         s2 = 0
@@ -1246,30 +1265,32 @@ class LevelEditor:
         elif dir == 'xy':
           s = round(renderer.RevX(mouse['x']))
           s2 = round(renderer.RevY(mouse['y']))
-          # print(s, s2, arg, arg2)
           setattr(self.ghost, arg, s)
           setattr(self.ghost, arg2, s2)
           renderer.text(
             10, 140, f"Click to set argument\nDirection: {dir}\nArgument: {arg}, {arg2}\nValue: {s}, {s2}", 'white')
 
+        # Update the 2D "mesh" of the object being placed
         if hasattr(self.ghost, 'updatePoints'):
           self.ghost.updatePoints()
 
+      # On left mouse click, place the object down
       if mouse['left']:
         mouse['left'] = False
-        # print(len(op)-3, self.argStep)
         if len(op) - 3 > self.argStep:  # keep going
           self.argStep += 1
         else:
           self.mode = "none"
           self.ghost = None
           self.argStep = 0
+          
+      # On right mouse click, cancel the object being placed
       elif mouse['right']:
         self.mode = "none"
         self.ghost = None
         self.argStep = 0
 
-      # Menu options on right side
+    # Menu options on right sidebar
     x = window['width'] - 50
     renderer.rect_outlined(
       window['width'] - 100, 5, window['width'] - 5, window['height'] - 5, 'black', 'white')
@@ -1278,19 +1299,18 @@ class LevelEditor:
       option = self.options[i]
       if self.mode == 'none' and mouse['x'] > x - 26 and mouse['x'] < x + 26 and mouse['y'] > y - 26 and mouse['y'] < y + 26:
         self.hovering = i
-
         mouse['cursor'] = 'hand2'
         if self.mode == 'none' and mouse['left']:
           mouse['left'] = False
           self.mode = 'placing'
           self.ghost = option[2](0, 0)
-
         renderer.rect(x - 22, y - 22, x + 22, y + 22, '#1a1')
       else:
         renderer.rect(x - 22, y - 22, x + 22, y + 22, '#222')
       if option[1]:
         renderer.img(option[1], x - 10, y - 10, 20)
       renderer.text_center(x, y - 17, option[0], 'white')
+      
       # Show how many options are available
       if len(option) > 3:
         renderer.text_center(
@@ -1298,13 +1318,8 @@ class LevelEditor:
       y += 50
 
 
-# Constants
-FPS = 30  # Frames per second
 
-####### Classes and global vars ########
-
-
-# Define root
+# Define root as a Tkinter window
 root = tk.Tk()
 
 # Define mouse position and keys down
@@ -1315,16 +1330,18 @@ window = {
   'width': 600,
   'height': 600
 }
-me = None
-bird2 = None
-bookOfAnswers = None
-ship2 = None
 
-# Define renderer
+# Global objects used throughout various objects in the game
+me = None # Initially player character (Bird), but can become an AllianceShip or TurretStation
+bird2 = None # Your companion, Albert
+bookOfAnswers = None # Book objects on the ground in the engine room
+ship2 = None # The ship that Albert jumps into
+
+# Define renderer and level editor
 renderer = renderer.Renderer(root, window)
 le = LevelEditor()
 
-
+# For debugging purposes only
 class DebugCircle():
   def __init__(self):
     objs.append(self)
@@ -1339,13 +1356,10 @@ class DebugCircle():
       self.x -= window['height'] * 1.5
 
   def draw(self):
-
-    # Draw the circle on the renderer
     renderer.world_circle(self.x, self.y, self.r, self.color)
 
+
 # Define UI elements
-
-
 class MenuText:
   def __init__(self, x, y, text, color, isVeryLarge=False):
     objs.append(self)
@@ -1399,7 +1413,6 @@ class MenuOption:
     colors = ['black', 'white']
     if self.hover:
       colors = ['#111', 'limegreen']
-
     renderer.rect_outlined(self.x - self.w / 2, self.y - self.h / 2,
                            self.x + self.w / 2, self.y + self.h / 2, colors[0], colors[1])
     renderer.text_center(self.x, self.y, self.text, "white")
@@ -1409,31 +1422,31 @@ class MenuOption:
     self.y = window['height'] / 2 + self.relativeY
 
 
-
 # Define game manager
-
-
 class GameManager:
 
   def __init__(self, root):
-    self.root = root
+    self.root = root # Reference to the Tkinter window
     self.resize_job = None
     self.scene_name = None
     self.showDebugInfo = False
 
-  # Thanks to GitHub Copilot
+  # When a resize is detected, call this function
   def on_possible_resize(self, event):
     if self.resize_job:
       self.root.after_cancel(self.resize_job)
     self.resize_job = self.root.after(
         200, lambda: self.on_certain_resize(event))
 
+  # When it is certain that the window has been resized, call this function
+  #   This is to prevent infinite loops of resizing
   def on_certain_resize(self, event):
     renderer.on_window_resize(event)
     for obj in objs:
       if hasattr(obj, 'resize'):
         obj.resize()
 
+  # On a scene change, call this
   def change_scene(self, scene):
     if self.scene_name == scene:
       return
@@ -1442,6 +1455,7 @@ class GameManager:
     w = window['width']
     h = window['height']
     if scene == "menu":
+      # Menu objects, yes the buttons and text are objects!
       MenuText(w / 2, h / 2 - 120, "Birds in Space", "#98F5F9", True)
       MenuOption("Start Game", w / 2, h / 2 - 20,
                  lambda: self.change_scene("game"))
@@ -1452,10 +1466,7 @@ class GameManager:
       MenuOption("Exit", w / 2, h / 2 + 130, lambda: self.root.quit())
     elif scene == "game":
 
-      # Game init
-      # for i in range(100):
-      #   DebugCircle()
-
+      # Insert Level Code Here!
       AllianceMotherShip(0, -12, 50, 25)
       RectRoom(-23, -60, 22, -51)
       RectRoom(-1, -51, 2, 27)
@@ -1500,7 +1511,6 @@ class GameManager:
       Decoration(-9, 21, 0)
       Decoration(-18, 21, 0)
       global me
-      # me = Bird(-18, -58)
       me = Bird(-14.0, 20.0, "bird_x.png", True)
       global bird2
       bird2 = Bird(-11.0, 19.0, "bird_y.png")
@@ -1519,10 +1529,8 @@ class GameManager:
       global bookOfAnswers
       bookOfAnswers = Decoration(14, 33, 3)
 
-
     elif scene == "level_editor":
-      # for i in range(5):
-      #   DebugCircle()
+      # Some scaffolding as a reference point
       AllianceMotherShip(0, -12, 50, 25)
       RectRoom(-23, -60, 22, -51)
       RectRoom(-1, -51, 2, 27)
@@ -1542,6 +1550,7 @@ class GameManager:
                "This game was created in 2 weeks in Python and Tkinter.", "white")
       MenuText(w / 2, h / 2 + 25, "Thanks for playing!", "white")
 
+  # Pre/post update for setting the cursor
   def preupdate(self):
     mouse['cursor'] = 'arrow'
 
@@ -1549,15 +1558,14 @@ class GameManager:
     renderer.label.config(cursor=mouse['cursor'])
 
 
+# Define the story controller and game manager
 storyController = StoryController()
 gameManager = GameManager(root)
 
-# Init, draw, main
 
 
 def init():
-  #
-
+  # TKinter setup
   root.title("|| Birds in Space ||")
   root.geometry(f"{window['width']}x{window['height']}")
   root.bind("<Configure>", gameManager.on_possible_resize)
@@ -1571,13 +1579,12 @@ def init():
   root.bind("<ButtonPress-3>", lambda e: mouse.update({'right': True}))
   root.bind("<ButtonRelease-3>", lambda e: mouse.update({'right': False}))
   root.bind("<MouseWheel>", lambda e: mouse.update({'scroll': e.delta}))
-
-  root.bind("<Escape>", lambda e: root.quit())
-  renderer.label.pack(fill='both', expand=True)
+  root.bind("<Escape>", lambda e: root.quit()) # Esc to close the window
+  renderer.label.pack(fill='both', expand=True) # For a resizable window
   gameManager.change_scene("menu")
 
 
-last_time = time.time()
+last_time = time.time() # To calculate deltatime
 
 
 def mainloop():
@@ -1626,6 +1633,7 @@ def mainloop():
   storyController.draw()
   if gameManager.scene_name == "level_editor":
     le.draw()
+    renderer.text(10, 200, "Press P to generate level code", "white")
 
   # Print debug (FPS)
   if gameManager.showDebugInfo:
